260ae5d4021a3bebb3c477f28cb732123de271be
wmz <wumz@kh-super.com>
2021-05-14 14:09:35 +0800

    修改demo使用展示

--- WMZPageController/DemoVC/复杂使用/淘宝/WMZTaoBaoDemo.m
+++ WMZPageController/DemoVC/复杂使用/淘宝/WMZTaoBaoDemo.m
@@ -76,7 +76,7 @@
            [strongSelf.upSc.lineView setHidden:YES];
        });
        self.param = param;
-       dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+       dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
            self.upSc.lineView.hidden = YES;
        });
 }
